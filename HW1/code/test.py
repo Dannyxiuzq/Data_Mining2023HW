a21b65b1d93a1629fd046b4330f91fad1c363f66
DannyXiu <75623709+Dannyxiuzq@users.noreply.github.com>
2023-03-23 22:57:05 +0800

    basic document

---
+++ HW1/code/test.py
@@ -0,0 +1,73 @@
+import os
+import glob
+import clip
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+_, preprocess = clip.load("ViT-B/32", device=device)
+model = torch.load('model.pkl')
+
+test_data_root = r'Project_Dataset/Selected_Test_Dataset/'
+path_list = os.listdir(test_data_root)
+all_image1_path = []
+all_image2_path = []
+for path in path_list:
+    image1_path = glob.glob(test_data_root + path + '/image1/' + '*.png')
+    image2_path = glob.glob(test_data_root + path + '/image2/' + '*.png')
+    all_image1_path.extend(image1_path)
+    all_image2_path.extend(image2_path)
+
+def get_txt_path(img_path):
+    i = 0
+    txt_path = ""
+    for s in img_path:
+        txt_path += s
+        if s == '/':
+            i += 1
+        if i == 3:
+            break
+    return txt_path
+
+class test_data(Dataset):
+    def __init__(self, img_path_list):
+        self.path = img_path_list
+
+    def __len__(self):
+        return len(self.path)
+
+    def __getitem__(self, item):
+        img = Image.open(self.path[item])
+        img = preprocess(img)
+        txt = get_txt_path(self.path[item]) + 'prompt.txt'
+        file = open(txt, encoding='utf-8')
+        prompt = file.read(300)
+        return img, prompt
+
+image1_dataset = test_data(all_image1_path)
+image2_dataset = test_data(all_image2_path)
+
+cor = 0
+with torch.no_grad():
+    """
+    测试过程：
+    取image1和image2的两张对应图片，分别计算[pgood1, pbad1]和[pgood2, pbad2], 如果pgood1 > pgood2 则 image1为good, 反之image2为good
+    """
+    for i in range(image1_dataset.__len__()):
+        img1 = image1_dataset[i][0].unsqueeze(0).to(device)
+        img2 = image2_dataset[i][0].unsqueeze(0).to(device)
+        txt = image1_dataset[i][1]
+        text = clip.tokenize(["a good photo of " + txt, "a bad photo of " + txt]).to(device)
+        image1_logits_per_image, image1_logits_per_text = model(img1, text)
+        image1_probs = image1_logits_per_image.softmax(dim=-1).cpu().numpy()
+        image2_logits_per_image, image2_logits_per_text = model(img2, text)
+        image2_probs = image2_logits_per_image.softmax(dim=-1).cpu().numpy()
+        if image1_probs[0][0] > image2_probs[0][0]:
+            print('y', end='')
+            cor += 1
+        else:
+            print('n', end='')
+
+print()
+print(cor, image1_dataset.__len__())
---
+++ HW1/code/train.py
@@ -0,0 +1,135 @@
+import os
+import glob
+import clip
+import torch
+import numpy as np
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader, random_split
+from PIL import Image
+
+"""加载clip模型"""
+device = "cuda" if torch.cuda.is_available() else "cpu"
+learning_rate = 5e-5
+model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+if device == "cpu":
+    model.float()
+else:
+    clip.model.convert_weights(model)
+
+"""读取数据集路径"""
+train_data_root = r'Project_Dataset/Selected_Train_Dataset/'
+path_list = os.listdir(train_data_root)
+all_good_images_path = []
+all_bad_images_path = []
+for path in path_list:
+    good_img_path = glob.glob(train_data_root + path + '/good/' + '*.png')
+    bad_img_path = glob.glob(train_data_root + path + '/bad/' + '*.png')
+    all_good_images_path.extend(good_img_path)
+    all_bad_images_path.extend(bad_img_path)
+
+def get_txt_path(img_path):
+    i = 0
+    txt_path = ""
+    for s in img_path:
+        txt_path += s
+        if s == '/':
+            i += 1
+        if i == 3:
+            break
+    return txt_path
+
+def convert_models_to_fp32(model):
+    for p in model.parameters():
+        p.data = p.data.float()
+        p.grad.data = p.grad.data.float()
+
+class train_data(Dataset):
+    def __init__(self, lable, img_path_list):
+        self.path = img_path_list
+        self.label = lable
+
+    def __len__(self):
+        return len(self.path)
+
+    def __getitem__(self, item):
+        """
+        :param item:
+        :return:
+            img:图片
+            txt:图片质量+图片描述
+            label:图片质量-good/bad 图片好/坏
+            prompt:图片描述-prompt.txt文本内容
+        """
+        img = Image.open(self.path[item])
+        img = preprocess(img)
+        txt = get_txt_path(self.path[item]) + 'prompt.txt'
+        file = open(txt, encoding='utf-8')
+        prompt = file.read(300)
+
+        if self.label == "good":
+            txt = "a beautiful and concise photo of " + prompt
+        else:
+            txt = "a mussy photo of " + prompt
+
+        # txt = "a " + self.label + " photo of " + prompt
+        return img, txt, self.label, prompt
+
+good_dataset = train_data("good", all_good_images_path)  # good图片数据集
+bad_dataset = train_data("bad", all_bad_images_path)  # bad图片数据集
+dataset = good_dataset + bad_dataset  # 形成整个数据集
+train_dataset, val_dataset, _ = random_split(dataset=dataset, lengths=[500, 100, 11480])#[9664, 2416])
+# train_dataset, val_dataset = random_split(dataset=dataset, lengths=[9664, 2416])  # 训练集和验证集划分 4:1
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=1, shuffle=True, num_workers=0)  # dataloader batch_size设置的小是因为显存不够
+val_dataloader = DataLoader(dataset=val_dataset, batch_size=1, shuffle=True, num_workers=0)
+
+loss_img = nn.CrossEntropyLoss().to(device)
+loss_txt = nn.CrossEntropyLoss().to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2)
+
+for i in range(10):
+
+    """训练过程"""
+    for batch, (img, txt, label, prompt) in enumerate(train_dataloader):
+        print('.', end='')
+        txts = clip.tokenize(txt).to(device)
+        imgs = img.to(device)
+        logits_per_image, logits_per_text = model(imgs, txts)
+
+        if device == "cpu":
+            ground_truth = torch.arange(1).long().to(device)
+        else:
+            ground_truth = torch.arange(1, dtype=torch.long, device=device)
+
+        total_loss = (loss_img(logits_per_image, ground_truth) + loss_txt(logits_per_text, ground_truth)) / 2
+        optimizer.zero_grad()
+        total_loss.backward()
+        if device == "cpu":
+            optimizer.step()
+        else:
+            convert_models_to_fp32(model)
+            optimizer.step()
+            clip.model.convert_weights(model)
+
+    print('[%d] loss: %.3f' % (i + 1, total_loss))
+
+    """验证过程"""
+    with torch.no_grad():
+        cor = 0
+        for batch, (img, txt, label, prompt) in enumerate(val_dataloader):
+            # text = clip.tokenize(["a good photo of " + prompt[0], "a bad photo of " + prompt[0]]).to(device)
+            # text = clip.tokenize([prompt[0], "other things"]).to(device)
+            # text = clip.tokenize(["a photo of " + prompt[0], "a photo without " + prompt[0]]).to(device)
+            # text = clip.tokenize(["a good photo of " + prompt[0] + "without other things", "a mess photo of " + prompt[0] + "with other things"]).to(device)
+            text = clip.tokenize(["a beautiful and concise photo of " + prompt[0], "a mussy photo of " + prompt[0]]).to(device)
+            image = img.to(device)
+            logits_per_image, logits_per_text = model(image, text)
+            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+            pred = np.argmax(probs)
+            """预测[good, bad]的概率[pgood, pbad],如果pgood > pbad 则预测为good, 反之为bad"""
+            if (pred == 0 and label[0] == "good") or (pred == 1 and label[0] == "bad"):
+                cor += 1
+            else:
+                print(txt, probs)
+        print(cor, '/', val_dataset.__len__(), ' = ', cor / val_dataset.__len__())
+
+    torch.save(model, 'model/model1-%s.pkl' % str(i))
