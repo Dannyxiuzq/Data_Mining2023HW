5ca3f0e432f939475c3787dc7bf2fafc7f66f1b2
OwwO99 <117959797+OwwO99@users.noreply.github.com>
2023-05-29 18:54:57 +0800

    Add files via upload

---
+++ HW1/code/CE_score.py
@@ -0,0 +1,168 @@
+import numpy as np
+import math
+import joblib
+import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from skimage.util import view_as_windows
+from joblib import Parallel, delayed
+'''
+def sliding_window(arr, window_size):
+    shape = (arr.shape[0] - window_size + 1, arr.shape[1] - window_size + 1, window_size, window_size)
+    strides = arr.strides + arr.strides
+    windows = np.lib.stride_tricks.as_strided(arr, shape=shape, strides=strides)
+    windows = windows.reshape(-1, window_size * window_size)
+    return windows
+'''
+
+
+def sliding_window(arr, window_size):
+    windows = view_as_windows(arr, (window_size, window_size))
+    windows = windows.reshape(-1, window_size * window_size)
+    return windows
+
+def permutation_entropy(windows):
+    k = windows.shape[0]
+    #print(k)
+    n = windows.shape[1]
+    flat_data = [d.reshape(-1) for d in windows]
+    sorted_data = [np.argsort(d) for d in flat_data]
+    #print(sorted_data)
+    count = {}
+    for s in sorted_data:
+        si = tuple(s)
+        if si in count.keys():
+            count[si] += 1
+        else:
+            count[si] = 1
+    #print(math.factorial(n))
+    counts = np.zeros((math.factorial(n),), dtype=int)
+    t = 0
+    for i in count.values():
+        counts[t] = i
+        t += 1
+    p = counts / len(sorted_data)
+    #print(p)
+    #return (-np.sum(p * np.log2(p + 1e-12))) / np.log2(math.factorial(n))
+    return p
+
+
+def shannon_entropy(p):
+    return -np.sum(p * np.log2(p + 1e-12))
+
+
+def n_shannon_entropy(p):
+    n = len(p)
+    return (shannon_entropy(p)) / np.log2(n)
+
+
+def JSdivergence(p):
+    n = len(p)
+    u = np.full(n, 1 / n)
+    h = (p + u) / 2
+    JS = shannon_entropy(h) - shannon_entropy(p) / 2 - shannon_entropy(u) / 2
+    return JS
+
+
+def statistical_complexity(p):
+    n = len(p)
+    d_star = -0.5 * (((n + 1) / n) * np.log2(n) + np.log2(n) - 2 * np.log2(2 * n))
+    complexity = JSdivergence(p) * n_shannon_entropy(p) / d_star
+    return complexity
+
+
+def cal_complexity_entropy(img, window_size=3):
+    #img = img.convert('L')
+    #img_g = np.asarray(img)
+    p = permutation_entropy(sliding_window(img, window_size))
+    entropy = n_shannon_entropy(p)
+    complexity = statistical_complexity(p)
+    return complexity, entropy
+
+
+def cal_complexity_entropy_rgb(img, window_size=3):
+    r, g, b = img.split()
+    img_r = np.asarray(r)
+    img_g = np.asarray(g)
+    img_b = np.asarray(b)
+    complexity_r, entropy_r = cal_complexity_entropy(img_r, window_size)
+    complexity_g, entropy_g = cal_complexity_entropy(img_g, window_size)
+    complexity_b, entropy_b = cal_complexity_entropy(img_b, window_size)
+    #p = permutation_entropy(sliding_window(img_g, window_size))
+    #entropy = n_shannon_entropy(p)
+    #complexity = statistical_complexity(p)
+    return complexity_r, entropy_r, complexity_g, entropy_g, complexity_b, entropy_b
+
+
+def get_ce_score(knn, svm, img, window_size=3):
+    cr, er, cg, eg, cb, eb = cal_complexity_entropy_rgb(img, window_size)
+    c = (cr + cg + cb) / 3
+    e = (er + eg + eb) / 3
+    #knn_score = knn.predict_proba([[c, e]])
+    score = svm.predict_proba([[c, e]])
+    #score = (knn_score + svm_score) / 2
+    return score
+
+
+'''
+def get_batch_ce_score(batch_path, knn, svm, window_size=3):
+    batch_ce_score = []
+    for i in range(len(batch_path)):
+        img_path = batch_path[i]
+        img = Image.open(img_path)
+        ce_score = get_ce_score(knn, svm, img, window_size)
+        batch_ce_score.append(ce_score)
+    return batch_ce_score
+'''
+
+
+def get_batch_ce_score(batch_path, knn, svm, window_size=3):
+    # Define a helper function to process a single image
+    def process_image(img_path):
+        img = Image.open(img_path)
+        return get_ce_score(knn, svm, img, window_size)
+
+    # Use parallel processing to process multiple images simultaneously
+    batch_ce_score = Parallel(n_jobs=-1)(delayed(process_image)(img_path) for img_path in batch_path)
+
+    return batch_ce_score
+
+def train(dataset, n=35, gamma=0.07, C=20):
+    c = []
+    e = []
+    y = []
+    for i in dataset:
+        good_img, bad_img, _, prompt = dataset[i]
+        goodc_r, goode_r, goodc_g, goode_g, goodc_b, goode_b = cal_complexity_entropy_rgb(good_img, 3)
+        badc_r, bade_r, badc_g, bade_g, badc_b, bade_b = cal_complexity_entropy_rgb(bad_img, 3)
+        goodc = (goodc_r + goodc_g + goodc_b) / 3
+        goode = (goode_r + goode_g + goode_b) / 3
+        badc = (badc_r + badc_g + badc_b) / 3
+        bade = (bade_r + bade_g + bade_b) / 3
+        c.append(goodc)
+        c.append(badc)
+        e.append(goode)
+        e.append(bade)
+        y.append(1)
+        y.append(0)
+    plt.scatter(c, e, c=y)
+    plt.show()
+    X = [[x1, x2] for x1, x2 in zip(c, e)]
+    knn = KNeighborsClassifier(n_neighbors=n)
+    knn.fit(X, y)
+    joblib.dump(knn, 'knn.plk')
+    svm = SVC(kernel='rbf', gamma=gamma, C=C, probability=True)
+    svm.fit(X, y)
+    joblib.dump(svm, 'svm.plk')
+
+
+#img_path = "Project_Dataset/Selected_Train_Dataset/a yellow t-shirt with a dog on it_/bad/a-yellow-t-shirt-with-a-dog-on-3.png"
+#img = Image.open(img_path)
+#img1 = img.convert('L')
+#gray_array = np.asarray(img1)
+#print(n_shannon_entropy(permutation_entropy(sliding_window(gray_array, 2))))
+#t = np.asarray([[6, 0, 2], [4, 5, 2], [6, 7, 4]])
+#print(permutation_entropy(sliding_window(t, 2)))
+#print(cal_complexity_entropy(img))
+#print(cal_complexity_entropy_rgb(img))
---
+++ HW1/code/D_score.py
@@ -0,0 +1,147 @@
+import numpy as np
+import spacy
+import pandas as pd
+import requests
+from PIL.Image import Image
+from transformers import ViltProcessor, ViltForQuestionAnswering, AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from PIL import Image
+
+
+nlp = spacy.load('en_core_web_sm')
+stop_words = spacy.lang.en.stop_words.STOP_WORDS
+color_list = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'brown', 'gray', 'black', 'white']
+num_list = {'a': 1, 'an': 1, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,
+            'nine': 9, 'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15,
+            'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19, 'twenty': 20, 'thirty': 30, 'forty': 40,
+            'fifty': 50, 'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90, 'hundred': 100, 'thousand': 1000,
+            'million': 1000000}
+processor = ViltProcessor.from_pretrained("model/vilt-b32-finetuned-vqa")
+model = ViltForQuestionAnswering.from_pretrained("model/vilt-b32-finetuned-vqa")
+QA = pipeline('question-answering', model="model/roberta-base-squad2", tokenizer="model/roberta-base-squad2")
+object_template = "Can you find any %s?"
+color_template = "Is the %s %s?"
+num_template = "%s is the number of %s?"
+relation_q_template = "Is %s?"
+relation_template = "What is the relation of '%s' and '%s'?"
+self_template = "What is the %s doing?"
+
+
+def object_extraction(objects):
+    objects_dict = {}
+    new_objects = []
+    for chunk in objects:
+        words = chunk.split(' ')
+        num = None
+        color = None
+        flag = 0
+        for i in range(len(words)):
+            if words[i] in num_list.keys():
+                num = num_list[words[i]]
+            elif words[i].isdigit():
+                num = int(words[i])
+            elif words[i] in color_list:
+                color = words[i]
+                flag = 1
+                break
+        object = ' '.join(words[i + flag:])
+        new_objects.append(object)
+        objects_dict[object] = {}
+        objects_dict[object]['Num'] = num
+        objects_dict[object]['Color'] = color
+    return objects_dict, new_objects
+
+
+def vqa(image, question, target):
+    encoding = processor(image, question, return_tensors="pt")
+    outputs = model(**encoding)
+    logits = outputs.logits.softmax(dim=-1).detach().numpy()
+    idx = model.config.label2id[target]
+    return logits[0][idx]
+
+
+def tqa(question, context):
+    QA_input = {
+        'question': question,
+        'context': context
+    }
+    res = QA(QA_input)
+    return res
+
+
+def get_matrix(prompt):
+    doc = nlp(prompt)
+    objects = [chunk.text for chunk in doc.noun_chunks if chunk.text not in stop_words]
+    objects_dict, objects = object_extraction(objects)
+    columns = ['Num', 'Color', 'Existence']
+    columns.extend(objects)
+    descriptive_matrix = pd.DataFrame.from_dict(objects_dict, orient='index')
+    score_matrix = pd.DataFrame(columns=columns, index=objects)
+    descriptive_matrix['Existence'] = None
+    for o in objects:
+        descriptive_matrix[o] = None
+    # print(descriptive_matrix)
+    for i in range(len(objects)):
+        descriptive_matrix.loc[objects[i], objects[i]] = tqa(self_template % objects[i], prompt)['answer']
+        for j in range(i + 1, len(objects)):
+            descriptive_matrix.loc[objects[i], objects[j]] = tqa(relation_template % (objects[i], objects[j]), prompt)[
+                'answer']
+    return descriptive_matrix, score_matrix
+
+
+def get_score(descriptive_matrix, score_matrix, image=None):
+    for i in descriptive_matrix.columns:
+        for j in descriptive_matrix.index:
+            if i == 'Existence':
+                question = object_template % j
+                score_matrix.loc[j, i] = vqa(image, question, 'yes')
+            elif i == 'Color' and descriptive_matrix.loc[j, i] is not None:
+                question = color_template % (j, descriptive_matrix.loc[j, i])
+                score_matrix.loc[j, i] = vqa(image, question, 'yes')
+            elif i == 'Num' and descriptive_matrix.loc[j, i] is not None:
+                #question = num_template % (descriptive_matrix.loc[j, i], j)
+                #score_matrix.loc[j, i] = vqa(image, question, 'yes')
+                pass
+            elif descriptive_matrix.loc[j, i] is not None:
+                question = relation_q_template % descriptive_matrix.loc[j, i]
+                score_matrix.loc[j, i] = vqa(image, question, 'yes')
+    return score_matrix
+
+
+def get_D_score(img, prompt):
+    descriptive_matrix, score_matrix = get_matrix(prompt)
+    score_matrix = get_score(descriptive_matrix, score_matrix, img)
+    describe_score = score_matrix.mean(axis=None)
+    if np.isnan(describe_score):
+        describe_score = 0
+    return describe_score
+
+
+def get_batch_score(batch_path, batch_prompt):
+    batch_describe_score = []
+    for i in range(len(batch_prompt)):
+        img_path = batch_path[i]
+        prompt = batch_prompt[i]
+        img = Image.open(img_path)
+        #descriptive_matrix, score_matrix = get_matrix(prompt)
+        describe_score = get_D_score(img, prompt)
+        batch_describe_score.append(describe_score)
+    return batch_describe_score
+
+
+'''
+img_path = "Project_Dataset/Selected_Train_Dataset/a yellow t-shirt with a dog on it_/bad/a-yellow-t-shirt-with-a-dog-on-3.png"
+img = Image.open(img_path)
+text = "a yellow t-shirt with a dog on it"
+#text = "beautiful fireworks in the sky with red, white and blue"
+#text = "Face of an orange frog in cartoon style"
+#text = "the words 'KEEP OFF THE GRASS'"
+#text = "a dream"
+descriptive_matrix, score_matrix = get_matrix(text)
+score_matrix = get_score(descriptive_matrix, score_matrix, img)
+print(score_matrix.mean(axis=None))
+#print(descriptive_matrix)
+#print(score_matrix)
+#print(get_score(descriptive_matrix, score_matrix, img))
+#print(vqa(img, 'Can you find any elephant?', 'yes'))
+'''
+
